016f0874d414b1f5218c82db32805ce8637edd17
Aline Vernier <aline.vernier@ensta.fr>
2019-01-25 11:38:14 +0100

    Added relative mode amplitude

--- LaserPlasmaAcc/SimpleFigures.py
+++ LaserPlasmaAcc/SimpleFigures.py
@@ -5,6 +5,9 @@ import numpy as np
 from scipy import constants as cs
 from scipy.integrate import odeint
 import pristinifier as ps
+import cmath as cm
+from matplotlib.widgets import Cursor
+
 
 ##############################################
 #             PATHS AND SETTINGS             #
@@ -19,27 +22,7 @@ matplotlib.rc('font', **font)
 
 my_path = "C:\Users\\vernier\PycharmProjects\octo-elective\\"
 
-##############################################
-#       OBJECT AND IMAGE SPACE               #
-##############################################
 
-fig = plt.figure(1, figsize=(10,10), dpi=80, facecolor='w', edgecolor='k', frameon=False)
-plt.xticks([])
-plt.yticks([])
-# Plot theta as a function of time
-ax1 = fig.add_subplot(111)
-
-ax1.fill_between(x, 0, 1,  facecolor='green', alpha=0.5)
-
-ax1.set_xlabel('$g1$', fontsize=30)
-ax1.set_ylabel('$g2$', fontsize=30)
-ax1.set_ylim(-1., 1.)
-ax1.set_xlim(-1., 1.)
-ax1.axhline(0, color='black')
-ax1.axvline(0, color='black')
-plt.tight_layout()
-#plt.show()
-#plt.savefig(my_path + 'espaces.png')
 
 
 ##############################################
@@ -100,6 +83,86 @@ plt.tight_layout()
 ##############################################
 #       SPECTRUM OF LONGITUDINAL MODES       #
 ##############################################
+c = 3e8
 
 
+wlambda = 632.8e-9
+f_mid = c/wlambda
+fmin = f_mid*(1.+6.e-6)
+fmax = f_mid*(1.-6.e-6)
 
+lw = f_mid*1e-8
+
+fnum = 10000
+fstep = (fmax - fmin)/fnum
+
+f_array = np.arange(fmin, fmax, fstep)
+
+
+R1 = 10.
+R2 = 20.
+L = 0.1
+
+p_mid = int(2*L/wlambda)
+
+A = 1-2L/R1
+B = 2L*(1-L/R2)
+z = L*(L-R2)/(R1+R2-2*L)
+zR = m.sqrt(L*(R1+R2-L)*(R1-L)*(R2-L)/(R1+R2-2*L)/(R1+R2-2*L))
+
+def w_sq(_z):
+        return zR*wlambda/3.14*(1+m.pow(_z/zR, 2))
+
+
+def dw(_p, _m, _n):
+        q_inv = 1 / R1 - 1j * wlambda / (3.14 * w_sq(z))
+        return c/(2*L)*(_p-(_n+_m+1)/(2*3.14)*cm.phase(A+B/q_inv))
+
+
+def lor(_f, _f0):
+        return 1/(1+pow((_f-_f0)/lw, 2))
+
+spectrum = np.zeros(fnum)
+p_lim = 4
+n_m_lim = 4
+for p_i in range(p_mid-p_lim, p_mid+p_lim):
+        for m_i in range(0, n_m_lim):
+                for n_i in range(0,1):
+                        for ii in range(0, fnum):
+                                spectrum[ii] += lor(f_array[ii], dw(p_i, m_i, n_i))\
+                                                *m.pow(1/(3.14*m.factorial(m_i)*m.factorial(n_i)), 0.5)\
+                                                *m.pow(2, -(n_i+m_i)/2)
+
+
+fig = plt.figure(1, figsize=(10, 6), dpi=80, facecolor='w', edgecolor='k')
+ax1 = fig.add_subplot(111)
+ax1.plot(f_array*1e-12, spectrum/max(spectrum), linewidth=2, color=tableau20[0])
+#cursor = Cursor(ax1, useblit=True, color='red', linewidth=1)
+
+ax1.set_xlabel('Frequence (THz)', fontsize=20)
+ax1.set_ylabel('Amplitude du mode (a.u.)', fontsize=20)
+ax1.set_ylim(0, 1.05)
+ax1.ticklabel_format(useOffset=False)
+ax1.set_xlim(fmin*1e-12, fmax*1e-12)
+plt.savefig(my_path + 'Mode Amplitude.png')
+
+
+#fig = plt.figure(1, figsize=(10,10), dpi=80, facecolor='w', edgecolor='k')
+
+# Plot theta as a function of time
+# ax1 = fig.add_subplot(111)
+# spectrum = np.zeros(fnum)
+# for p_i in np.arange(1.24e7, 1.24e7, 1):
+#         for n_i in range(0,1,1):
+#                 for m_i in range(0, 1, 1):
+#                         for i_i in range(0, fnum):
+#                                 spectrum[i_i] += lor(f_array[i_i], dw(p_i, m_i, n_i))
+#
+#
+# ax1.plot(f_array, spectrum, linewidth=2, color=tableau20[0])
+#
+# #ax1.set_xlabel('$x$', fontsize=30)
+# #ax1.set_ylabel('$\\frac{p^2(t)}{m\omega_0}$', fontsize=30)
+# plt.tight_layout()
+# plt.show()
+#plt.savefig(my_path + 'simple_phase_space.png')
